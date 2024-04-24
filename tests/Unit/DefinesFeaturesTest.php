be6f9975ce2477d2cbaaf8cc350c84c5f015d21d
Fabio Ivona <fabio.ivona@defstudio.it>
2024-04-25 00:04:54 +0200

    wip
    Took 2 minutes

--- tests/Unit/DefinesFeaturesTest.php
+++ tests/Unit/DefinesFeaturesTest.php
@@ -1,9 +1,12 @@
 <?php
 
 use DefStudio\EnumFeatures\Tests\Fixtures\Feature;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User;
 
 test('defines_feature', function () {
+    Model::unguard();
+
     $enabled = new User([
         'id' => 1,
         'name' => 'enabled_user',
